04eedff3b605f309a220c269c63d5e302675bb96
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-12-06 02:35:56 +0000

    DAy21

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ var computerChoice= "";
 var winner = "";
 var randomNumber = getRandomNum();
 function getRandomNum(){
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * 4);
 }
 $("#shoot").click(function(){
     $("#userChoice").text($("#input").val());
@@ -22,11 +22,11 @@ $("#shoot").click(function(){
 
 $("#shoot").click(function(){
     alert(getRandomNum());
-    if(randomNumber > 1 && randomNumber < 3){
-        $("#computerChoice").text("rock");
+    if(randomNumber === 1 ){
+        $("#computerChoice").html("rock");
     } 
-    else if(randomNumber > 4 && randomNumber < 7){
-        $("#computerChoice").text("paper");
+    else if(randomNumber === 2){
+        $("#computerChoice").append("paper");
     }else{
          $("#computerChoice").text("scissors");
     }
